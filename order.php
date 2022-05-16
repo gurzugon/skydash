97dabb5763b6cf0fadf6abfc5c8af96b5d057db0
gurzugon <rushsyahrin@gmail.com>
2022-05-16 13:31:12 +0800

    Initial commit

--- order.php
+++ order.php
@@ -372,8 +372,8 @@ if (mysqli_num_rows($res_u) > 0 ) {
 
   <!--popup form add order-->
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content" style="width: 702px;height: 752px;">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Add Order</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
